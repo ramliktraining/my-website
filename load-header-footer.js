512600228a8caf4d58952cca9c808132870496ef
ramliktraining <ramliktraining@gmail.com>
2025-06-05 12:18:59 +0400

    Add files via upload

---
+++ load-header-footer.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Load header
+  fetch("header.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    });
+
+  // Load footer
+  fetch("footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    });
+});
