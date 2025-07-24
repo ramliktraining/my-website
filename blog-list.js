eb2bdd6536c1c76730477f07717081491f5c787b
ramliktraining <ramliktraining@gmail.com>
2025-07-24 08:20:38 +0400

    blog-list.js

--- blog-list.js
+++ blog-list.js
@@ -1,16 +1,18 @@
 // blog-list.js
-fetch('posts/index.json')
-  .then(res => res.json())
+fetch('posts.json')
+  .then(response => response.json())
   .then(posts => {
-    const listContainer = document.getElementById('blog-list');
-    posts.forEach(post => {
-      const item = document.createElement('article');
-      item.className = 'blog-preview';
-      item.innerHTML = `
-        <h2><a href="${post.url}">${post.title}</a></h2>
-        <p>${post.description}</p>
-        <a href="${post.url}" class="cta-button">Read More</a>
-      `;
-      listContainer.appendChild(item);
-    });
+    const blogList = document.getElementById('blog-list');
+    blogList.innerHTML = posts.map(post => `
+      <article class="blog-preview">
+        <h2><a href="blog-post.html?slug=${post.slug}">${post.title}</a></h2>
+        <p>${post.description || ''}</p>
+        <p><small>${post.date || ''}</small></p>
+        <a href="blog-post.html?slug=${post.slug}" class="cta-button">Read More</a>
+      </article>
+    `).join('');
+  })
+  .catch(err => {
+    console.error('Error loading blog posts:', err);
+    document.getElementById('blog-list').innerHTML = "<p>Failed to load blog posts.</p>";
   });
